9bd73352be3b79ac08f19861da80e74440f74e4e
kartikchauhan <chauhan.kartik25@gmail.com>
2023-01-03 01:42:18 +0530

    create album repository and save entity

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-import { Client, Entity } from 'redis-om'
+import { Client } from 'redis-om'
+
+import { albumSchema, studioSchema } from './Entity/index.js'
 
 const client = new Client()
 await client.open('redis://localhost:6379')
 
-const aString = await client.execute(['PING'])
-// 'PONG'
+const albumRepository = client.fetchRepository(albumSchema)
+const studioRepository = client.fetchRepository(studioSchema)
 
-const aNumber = await client.execute(['HSET', 'foo', 'bar', 'baz', 'qux', 42])
-// 2
+const album = albumRepository.createEntity()
+album.artist = 'Linkin Park'
+album.title = 'Living Things'
+album.year = 2012
+album.genres = ['Alternative rock', 'Alternative/Indie', 'Metal']
+album.multipleArtists = true
 
-const anArray = await client.execute(['HGETALL', 'foo'])
-// [ 'bar', 'baz', 'qux', '42' ]
-console.log(anArray)
+const id = await albumRepository.save(album)
+
+/**
+ * Meta Info
+ * TYPE Album:01GNT1AQ852JB17MSSQQ9K6G0Z => ReJSON-RL
+ * JSON.OBJLEN Album:01GNT1AQ852JB17MSSQQ9K6G0Z => (integer) 5
+ * JSON.OBJKEYS Album:01GNT1AQ852JB17MSSQQ9K6G0Z =>
+ *  1) "artist"
+    2) "title"
+    3) "year"
+    4) "genres"
+    5) "multipleArtists"
+    JSON.GET Album:01GNT1AQ852JB17MSSQQ9K6G0Z artist => "\"Linkin Park\""
+ */
+// console.log(album.entityId) // 01GNT1AQ852JB17MSSQQ9K6G0Z
+// console.log(id) // 01GNT1AQ852JB17MSSQQ9K6G0Z
 
 await client.close()
