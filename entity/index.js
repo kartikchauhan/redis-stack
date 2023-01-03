c02c2d6934cc1d7e1c36a80febf4a7db7ae9c91e
kartikchauhan <chauhan.kartik25@gmail.com>
2023-01-04 00:33:54 +0530

    import and export artistSchema

--- entity/index.js
+++ entity/index.js
@@ -1,7 +1,9 @@
 import { albumSchema } from './album.js';
+import { artistSchema } from './artist.js';
 import { studioSchema } from './studio.js';
 
 export {
     albumSchema,
+    artistSchema,
     studioSchema,
 }
