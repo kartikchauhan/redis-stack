fd63d824ff6c0abb3453171a22d36c02fa9ebbd2
kartikchauhan <chauhan.kartik25@gmail.com>
2023-01-04 00:33:29 +0530

    move all operations in a single async function

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import { Client } from 'redis-om'
 
 import albumUtil from './utils/album.js'
+import artistutil from './utils/artist.js'
 import studioUtil from './utils/studio.js'
 
 const client = new Client()
 await client.open('redis://localhost:6379')
 
-albumUtil.create(client)
-studioUtil.create(client)
 
-await client.close()
+async function exec() {
+    // albumUtil.create(client)
+
+    // studioUtil.create(client)
+
+    const artistId = await artistutil.create(client)
+
+    const artist = await artistutil.fetch(client, artistId)
+
+    console.log(artist)
+    
+    await client.close()
+}
+
+exec()
+
+
