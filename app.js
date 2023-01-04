86743533c86bb21f4f87a29a579606ccd521609a
kartikchauhan <chauhan.kartik25@gmail.com>
2023-01-05 01:25:02 +0530

    add try/catch block

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { Client } from 'redis-om'
 
 import albumUtil from './utils/album.js'
-import artistutil from './utils/artist.js'
+import artistUtil from './utils/artist.js'
 import studioUtil from './utils/studio.js'
 
 const client = new Client()
@@ -9,17 +9,26 @@ await client.open('redis://localhost:6379')
 
 
 async function exec() {
-    // albumUtil.create(client)
+    try {
 
-    // studioUtil.create(client)
+        // albumUtil.create(client)
 
-    const artistId = await artistutil.create(client)
+        // studioUtil.create(client)
 
-    const artist = await artistutil.fetch(client, artistId)
+        // const artistId = await artistUtil.create(client)
 
-    console.log(artist)
-    
-    await client.close()
+        // const artist = await artistUtil.fetch(client, artistId)
+
+        // console.log(artist)
+
+        await artistUtil.createIndex(client)
+
+        await artistUtil.search(client)
+    } catch (err) {
+        console.log('Error', err)
+    } finally {
+        await client.close()
+    }
 }
 
 exec()
